cea8efa770239cafc7d618fb16a8ff87ca8b2299
Justin Freitag <justin.freitag@gmail.com>
2014-06-29 19:49:46 +1000

    Increased test coverage

--- test/test.js
+++ test/test.js
@@ -14,16 +14,24 @@ beforeEach(function (done) {
   client.on('connect', done);
 });
 
-it('should fail to authenticate', function (done) {
- client.call('AUTH', 'test', function (error, result) {
+it('should fail on unknown command', function (done) {
+ client.call('FOO', 'bar', function (error, result) {
     assert(error instanceof Error);
-    assert.equal(result, null);
+    assert.equal(result, undefined);
+    done();
+  });
+});
+
+it('should fail to authenticate', function (done) {
+ client.call('AUTH', 'foo', function (error, result) {
+    assert(error instanceof Error);
+    assert.equal(result, undefined);
     done();
   });
 });
 
 it('should not catch callback error', function (done) {
-  var errorMessage = 'POOOONG!';
+  var errorMessage = 'FOO';
   var d = domain.create();
   d.on('error', function (error) {
     assert.equal(error.message, errorMessage);
@@ -34,6 +42,17 @@ it('should not catch callback error', function (done) {
   client.call('PING', function (error, result) {
     throw new Error(errorMessage);
   });
+});
+
+it('should emit error in place of call-error', function (done) {
+  var d = domain.create();
+  d.on('error', function (error) {
+    assert(error instanceof Error);
+    d.dispose();
+    done();
+  });
+  d.enter();
+  client.call('FOO');
 });
 
 it('should set and get value', function (done) {
@@ -48,7 +67,7 @@ it('should set and get value', function (done) {
 
 it('should multi/exec', function (done) {
   client.call('MULTI');
-  client.call('SETEX', 'foo', '5000000', 'bar');
+  client.call('SETEX', 'foo', 5000000, 'bar');
   client.call('RPUSH', 'listfoo', 'bar');
   client.call('SADD', 'setfoo', 'bar');
   client.call('EXEC', function (error, result) {
@@ -59,24 +78,39 @@ it('should multi/exec', function (done) {
   });
 });
 
-it('should multi/exec with error', function (done) {
+it('should multi/exec with error in result', function (done) {
   client.call('MULTI');
-  client.call('AUTH', 'error');
-  client.call('SET', 'foo', 'multibar');
+  client.call('AUTH', 'foo');
+  client.call('SET', 'foo', 'bar');
   client.call('GET', 'foo');
   client.call('EXEC', function (error, result) {
     assert(result[0] instanceof Error);
     assert.equal(result[1], 'OK');
-    assert.equal(result[2], 'multibar');
+    assert.equal(result[2], 'bar');
     done();
   });
 });
 
-it.skip('should emit call-error', function (done) {
+it('should abort multi/exec', function (done) {
+  client.once('call-error', function (error) {
+    assert(error instanceof Error);
+  });
   client.call('MULTI');
+  client.call('FOO');
+  client.call('SET', 'foo', 'bar');
   client.call('EXEC', function (error, result) {
+    assert(error instanceof Error);
+    assert.equal(result, undefined);
     done();
   });
+});
+
+it('should emit call-error', function (done) {
+  client.on('call-error', function (error) {
+    assert(error instanceof Error);
+    done();
+  });
+  client.call('FOO');
 });
 
 it('should fail to publish when in subscribe mode', function (done) {
@@ -91,9 +125,28 @@ it('should fail to publish when in subscribe mode', function (done) {
   });
 });
 
+it('should return undefined result on blpop timeout', function (done) {
+  client.call('BLPOP', 'listbar', 1, function (error, result) {
+    assert.equal(error, undefined);
+    assert.equal(result, undefined);
+    done();
+  });
+});
+
 it('should quit', function (done) {
   client.on('close', done);
   client.quit();
+});
+
+it('should quit on blpop', function (done) {
+  client.once('error', function (error) {
+    assert(error instanceof Error);
+    client.on('error', function (ignore) {});
+    done();
+  });
+  client.call('BLPOP', 'listbar', 0);
+  client.quit();
+  clock.tick(3000);
 });
 
 it('should timeout on quit', function (done) {
